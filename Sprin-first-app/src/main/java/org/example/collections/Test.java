c7fa700cd463f74bc3d9d1a67a32623590264dc8
Sharma <kanisha@apac.corpdir.net>
2023-02-27 12:41:32 +0530

    injecting Collection Types Sets,Maps,List

---
+++ Sprin-first-app/src/main/java/org/example/collections/Test.java
@@ -0,0 +1,16 @@
+package org.example.collections;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+    public static void main(String[] args) {
+       ApplicationContext context = new ClassPathXmlApplicationContext("collectionconfig.xml");
+        Emp emp1= (Emp) context.getBean("emp1");
+        System.out.println(emp1.getName());
+        System.out.println(emp1.getAddreses());
+        System.out.println(emp1.getPhones());
+        System.out.println(emp1.getCourses());
+
+    }
+}
